cab994e7d63fa0bc8537d698d74c2f4eb77199b3
Elisheba <leighw33@gmail.com>
2020-07-23 06:59:56 -0400

    debugging why create table is throwing an error

--- src/main/resources/db/migration/V1__create_tables.sql
+++ src/main/resources/db/migration/V1__create_tables.sql
@@ -1,16 +1,16 @@
-CREATE TABLE user (
-    id INT NOT NULL PRIMARY KEY,
-    role VARCHAR(50) NOT NULL,
-    location VARCHAR(50) NOT NULL,
-    status VARCHAR(50) NOT NULL,
-    maintenance_id INT NOT NULL
-);
-
-CREATE TABLE maintenance_requests (
- id INT NOT NULL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS maintenance_requests(
+ user_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
  location VARCHAR NOT NULL,
  status VARCHAR NOT NULL,
  date_witnessed TIMESTAMP NOT NULL,
  description VARCHAR(500) NOT NULL,
  photo BLOB
+)
+
+CREATE TABLE IF NOT EXISTS user(
+    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role VARCHAR(50) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    maintenance_id INT NOT NULL
 )
