85d6d195665bec92578613e31bd42261d0c199b2
Elisheba <leighw33@gmail.com>
2020-07-30 07:06:09 -0400

    Adding app user entity

--- src/main/java/com/maintenance/requests/entity/AppUsers.java
+++ src/main/java/com/maintenance/requests/entity/AppUsers.java
@@ -1,10 +1,30 @@
 package com.maintenance.requests.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
 @Entity
-@Table(name = "app_users")
+@Getter 
+@Setter 
+@NoArgsConstructor 
 public class AppUsers {
+	
+	@Id
+	private int userId;
+	@Column
+	private String role;
+	@Column
+	private String location;
+	@Column
+	private byte photo;
+	
 
 }
