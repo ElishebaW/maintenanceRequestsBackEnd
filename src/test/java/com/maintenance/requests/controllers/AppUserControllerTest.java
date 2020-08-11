19768794b1f9fcaddd85571b5cdd44e08732ae8f
Elisheba <leighw33@gmail.com>
2020-08-11 06:40:37 -0400

    Working on test

--- src/test/java/com/maintenance/requests/controllers/AppUserControllerTest.java
+++ src/test/java/com/maintenance/requests/controllers/AppUserControllerTest.java
@@ -1,16 +1,31 @@
 package com.maintenance.requests.controllers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class AppUserControllerTest {
 	
 	@Autowired
-	private MockMvc mockmvc;
+	private MockMvc mockMvc;
 
 	@Test
-	public void testGetAppUsersController() {
+	public void testGetAppUsersController() throws Exception {
+		
+		mockMvc
+		.perform(get("/appUsers/1"))
+		.andExpect(status().isOk());
 		
 	}
 
