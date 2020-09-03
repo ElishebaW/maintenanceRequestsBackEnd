88d8485304f1334792d689c4c97d96d442da2154
Elisheba <leighw33@gmail.com>
2020-09-03 07:44:05 -0400

    Adding tests

--- src/test/java/com/maintenance/requests/MaintenanceRequestsTests.java
+++ src/test/java/com/maintenance/requests/MaintenanceRequestsTests.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MaintenanceRequestsTests {
+public class MaintenanceRequestsTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
---
+++ src/test/java/com/maintenance/requests/controllers/MaintenaceRequestsControllerTest.java
@@ -0,0 +1,14 @@
+package com.maintenance.requests.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MaintenaceRequestsControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
